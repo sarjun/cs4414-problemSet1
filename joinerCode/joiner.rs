f74b05d907c741f84c7fb2a235d7a2457c336529
Arjun Shankar <sarjun93@gmail.com>
2014-01-26 12:52:30 -0500

    Fixed some bugs. Now it works

--- joinerCode/joiner.rs
+++ joinerCode/joiner.rs
@@ -1,34 +1,35 @@
-use std::rand::random;
 use std::os;
 use std::io::File;
+use std::str;
 
 fn main() {
     let args: ~[~str] = os::args();
+    let argsClone = args.clone();
     if args.len() != 3 {
         println!("Usage: {:s} <splitFile1> <splitFile2>", args[0]); 
     } else {
         let f1name = args[1];
-        let f2name = args[2];
+        let f2name = argsClone[2];
         let path1 = Path::new(f1name.clone());
         let path2 = Path::new(f2name.clone());
         let msg_file1 = File::open(&path1);
         let msg_file2 = File::open(&path2);
 
         match (msg_file1, msg_file2) {
-            Some(mut msg1, mut msg2) => {
+            (Some(mut msg1), Some(mut msg2)) => {
                 let msg1_bytes: ~[u8] = msg1.read_to_end();
                 let msg2_bytes: ~[u8] = msg2.read_to_end();
                 let output_file
                        = File::create(&Path::new("originalMsg"));
                 
                 match (output_file) {
-                    (Some(output)) => { 
-                        split(msg1_bytes, msg2_bytes, output_file); 
+                    Some(output) => { 
+                        split(msg1_bytes, msg2_bytes, output); 
                         } ,
-                    (_, _) => fail!("Error opening output files!"),
+                    _ => fail!("Error opening output files!"),
                 }
             } ,
-            None => fail!("Error opening message file: {:s}", fname)
+            (_, _) => fail!("Error opening split files: {:s}, {:s}", f1name, f2name)
         }
     }
 }
@@ -41,7 +42,8 @@ fn xor(a: &[u8], b: &[u8]) -> ~[u8] {
     ret
 }
 
-fn split(msg1_bytes: &[u8], mut msg2_bytes: &[u8], mut output_file: File) {
-    let mut original_bytes: ~[u8] = xor(msg1_bytes, msg2_bytes);
+fn split(msg1_bytes: &[u8], msg2_bytes: &[u8], mut output_file: File) {
+    let original_bytes: ~[u8] = xor(msg1_bytes, msg2_bytes);
     output_file.write(original_bytes);
+    print(str::from_utf8(original_bytes));
 }
